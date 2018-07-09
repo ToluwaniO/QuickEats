65478f0b91b3116c6ae175394c238b88c8187409
ToluwaniO <ogunsanyatoluwani@gmail.com>
2018-07-09 17:54:09 -0400

    added kotlin

---
+++ app/src/main/java/toluog/quickeats/model/Restaurant.kt
@@ -0,0 +1,4 @@
+package toluog.quickeats.model
+
+
+data class Restaurant(var id: String = "", var name: String = "", var imageUrl: String = "")
---
+++ app/src/main/java/toluog/quickeats/model/Table.kt
@@ -0,0 +1,4 @@
+package toluog.quickeats.model
+
+data class Table(var id: String = "", var restaurantId: String = "", var isOccupied: Boolean = false,
+                 var total: Double = 0.0)
